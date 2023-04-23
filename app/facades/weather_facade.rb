b672703ca418ad5a8cefedb9f32dadb0230bba93
Andra Helton <andrahelton@gmail.com>
2023-04-23 14:39:39 -0600

    fix: Adjust methods for one location argument

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -3,32 +3,33 @@ class WeatherFacade
     @service = WeatherService.new
   end
   
-  def forecast_json(lat, long)
-    @service.fiveday_forecast_coords(lat, long)
+  def forecast_json(location)
+    coords = MapQuestFacade.new.get_coords(location)
+    @service.fiveday_forecast_coords(coords[:lat], coords[:lng])
   end
 
-  def current_weather(lat, long)
-    CurrentWeather.new(forecast_json(lat, long)[:current])
+  def current_weather(location)
+    CurrentWeather.new(forecast_json(location)[:current])
   end
 
-  def daily_weather(lat, long)
-    json = forecast_json(lat, long)[:forecast][:forecastday]
+  def daily_weather(location)
+    json = forecast_json(location)[:forecast][:forecastday]
     json.map do |day|
       DailyWeather.new(day)
     end
   end
 
-  def hourly_weather(lat, long)
-    days = forecast_json(lat, long)[:forecast][:forecastday]
+  def hourly_weather(location)
+    days = forecast_json(location)[:forecast][:forecastday]
     days.first[:hour][-12, 8].map do |hour|
       HourlyWeather.new(hour)
     end
   end
 
-  def full_forecast(lat, long)
-    current = current_weather(lat, long)
-    daily = daily_weather(lat, long)
-    hourly = hourly_weather(lat, long)
+  def full_forecast(location)
+    current = current_weather(location)
+    daily = daily_weather(location)
+    hourly = hourly_weather(location)
 
     Forecast.new(current, daily, hourly)
   end
--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe WeatherFacade do
   describe "current", :vcr do
-    let(:current) { WeatherFacade.new.current_weather("39.742043", "-104.991531") }
+    let(:current) { WeatherFacade.new.current_weather("Denver, CO") }
    
     it "creates a poros for current weather info by location" do
       expect(current).to be_a(CurrentWeather)
@@ -10,7 +10,7 @@ RSpec.describe WeatherFacade do
   end
 
   describe "daily", :vcr do
-    let(:days) { WeatherFacade.new.daily_weather("39.742043", "-104.991531") }
+    let(:days) { WeatherFacade.new.daily_weather("Denver, CO") }
    
     it "creates a poros for daily weather info by location" do
       day = days.first
@@ -19,7 +19,7 @@ RSpec.describe WeatherFacade do
   end
 
   describe "hourly", :vcr do
-    let(:hours) { WeatherFacade.new.hourly_weather("39.742043", "-104.991531") }
+    let(:hours) { WeatherFacade.new.hourly_weather("Denver, CO") }
    
     it "creates a poros for hourly weather info by location" do
       hour = hours.first
@@ -29,7 +29,7 @@ RSpec.describe WeatherFacade do
   end
 
   describe "full_forecast", :vcr do
-    let(:forecast) { WeatherFacade.new.full_forecast("39.742043", "-104.991531") }
+    let(:forecast) { WeatherFacade.new.full_forecast("Denver, CO") }
   
     it "creates a poros for full forecast info by location" do
       expect(forecast).to be_a(Forecast)
