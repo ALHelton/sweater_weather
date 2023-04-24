a5d2cf22e627408dd01f57b894dc26bbecc06e41
Andra Helton <andrahelton@gmail.com>
2023-04-24 11:14:02 -0600

    Add facade test adjustment

--- spec/facades/teleport_facade_spec.rb
+++ spec/facades/teleport_facade_spec.rb
@@ -5,13 +5,23 @@ RSpec.describe TeleportFacade do
     let(:salary) { TeleportFacade.new.get_city_salaries("chicago") }
 
     it "creates a poros for salary info by city" do
+      
+      specific_titles = [
+        "Data Analyst", 
+        "Data Scientist", 
+        "Mobile Developer", 
+        "QA Engineer",
+        "Software Engineer",
+        "Systems Administrator",
+        "Web Developer"
+      ]
+
       expect(salary).to be_a(Salary)
       expect(salary.destination).to be_a(String)
       expect(salary.forecast).to be_a(Hash)
       expect(salary.forecast.keys).to eq([:summary, :temperature])
       expect(salary.forecast[:summary]).to be_a(String)
       expect(salary.forecast[:temperature]).to be_a(String)
-
       expect(salary.id).to eq(nil)
       expect(salary.salaries).to be_an(Array)
 
@@ -19,6 +29,7 @@ RSpec.describe TeleportFacade do
         expect(job).to be_a(Hash)
         expect(job.keys).to eq([:title, :min, :max])
         expect(job[:title]).to be_a(String)
+        expect(specific_titles).to include(job[:title])
         expect(job[:min]).to be_a(String)
         expect(job[:max]).to be_a(String)
       end
