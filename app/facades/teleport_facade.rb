5377b8a1537785cb9f6c993ccf9fbe8d77c8f105
Andra Helton <andrahelton@gmail.com>
2023-04-24 10:19:49 -0600

    Add working on the facade

--- app/facades/teleport_facade.rb
+++ app/facades/teleport_facade.rb
@@ -6,15 +6,24 @@ class TeleportFacade
 
   def get_city_salaries(city)
     all_salaries = @service.city_salaries(city)[:salaries]
+    entire_forecast = WeatherFacade.new.full_forecast(city)
 
     destination = "#{city}"
+
+    forecast = {
+      summary: entire_forecast.hourly_weather.first[:conditions],
+      temperature: "#{entire_forecast.current_weather[:temperature]} F"
+    }
+    require 'pry'; binding.pry
 
     salaries = all_salaries.map do |salary|
       {
         title: salary[:job][:title],
-        min: sprintf("$%.2f", salary[:salary_percentiles][:percentile_25], delimeter: ",")
+        min: sprintf("$%.2f", salary[:salary_percentiles][:percentile_25], delimeter: ","),
         max: sprintf("$%.2f", salary[:salary_percentiles][:percentile_75], delimeter: ",")
       }
     end
+
+    Salary.new(destination, forecast, salaries)
   end
 end
