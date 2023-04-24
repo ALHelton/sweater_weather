036192a06b2051e687b3c8beae8d149eb1bc7615
Andra Helton <andrahelton@gmail.com>
2023-04-24 09:35:38 -0600

    Add service test

--- spec/services/teleport_service_spec.rb
+++ spec/services/teleport_service_spec.rb
@@ -6,6 +6,38 @@ RSpec.describe TeleportService do
 
     it "establishes a connection to get forcast by coordinates" do
       expect(service).to be_a(Hash)
+      expect(service.keys).to eq([:_links, :salaries])
+      
+      expect(service[:_links]).to be_a(Hash)
+      expect(service[:_links][:curies]).to be_an(Array)
+      expect(service[:_links][:curies][0]).to be_a(Hash)
+      expect(service[:_links][:curies][0]).to have_key(:name)
+      expect(service[:_links][:curies][0][:name]).to be_a(String)
+
+      expect(service[:salaries]).to be_an(Array)
+      expect(service[:salaries][0]).to be_a(Hash)
+      expect(service[:salaries][0].keys).to eq([:job, :salary_percentiles])
+      expect(service[:salaries][0][:job]).to be_a(Hash)
+      expect(service[:salaries][0][:job]).to have_key(:salary_percentiles)
+      expect(service[:salaries][0][:job][:salary_percentiles]).to be_a(Hash)
+      expect(service[:salaries][0][:job][:salary_percentiles]).to have_key(:percentile_25)
+      
+      expect(service[:salaries][0][:job][:salary_percentiles]).to have_key(:percentile_25)
+      expect(service[:salaries][0][:job][:salary_percentiles][:percentile_25]).to be_a(Float)
+      expect(service[:salaries][0][:job][:salary_percentiles]).to have_key(:percentile_75)
+      expect(service[:salaries][0][:job][:salary_percentiles][:percentile_75]).to be_a(Float)
+
+
+
+
+
+
+
+
+
+
+
+
       require 'pry'; binding.pry
     end
   end
