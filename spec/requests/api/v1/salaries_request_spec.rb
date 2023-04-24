d52221090bbfca2828f28fc8e793e90a2294f89b
Andra Helton <andrahelton@gmail.com>
2023-04-24 11:01:55 -0600

    Add finished request testing

--- spec/requests/api/v1/salaries_request_spec.rb
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -6,10 +6,26 @@ RSpec.describe "Salaries Request" do
       get "/api/v1/salaries?destination=chicago"
       parsed = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
-      # require 'pry'; binding.pry
+
       expect(parsed[:data]).to be_a(Hash)
       expect(parsed[:data].keys).to eq([:id, :type, :attributes])
+      expect(parsed[:data][:type]).to eq("salaries")
+      expect(parsed[:data][:attributes]).to be_a(Hash)
+      expect(parsed[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
 
+      expect(parsed[:data][:attributes][:destination]).to be_a(String)
+      expect(parsed[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(parsed[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+      expect(parsed[:data][:attributes][:forecast][:summary]).to be_a(String)
+      expect(parsed[:data][:attributes][:forecast][:temperature]).to be_a(String)
+
+      expect(parsed[:data][:attributes][:salaries]).to be_an(Array)
+      expect(parsed[:data][:attributes][:salaries].first).to be_a(Hash)
+      expect(parsed[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
+
+      expect(parsed[:data][:attributes][:salaries].first[:title]).to be_a(String)
+      expect(parsed[:data][:attributes][:salaries].first[:min]).to be_a(String)
+      expect(parsed[:data][:attributes][:salaries].first[:max]).to be_a(String)
     end
   end
 end
