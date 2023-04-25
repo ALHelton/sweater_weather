d2c2afea2619d0241f3c665afdea3597b6ccac50
Andra Helton <andrahelton@gmail.com>
2023-04-24 22:28:33 -0600

    feat: UsersController#create sad path

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    user_params[:email] = user_params[:email].downcase
     user = User.new(user_params)
-
-    if user.password == user.password_confirmation
-      user.save
+    if user.save
+      user.email = user_params[:email].downcase
       render json: UserSerializer.new(user), status: 201
+    else
+      render json: { error: user.errors.full_messages.to_sentence }, status: 400
     end
   end
 
