294fb804da0cf0f7c84c1e8feba0c45b39948171
Andra Helton <andrahelton@gmail.com>
2023-04-25 13:36:00 -0600

    test: Session Request User Login sad path

--- spec/requests/api/v0/session_request_spec.rb
+++ spec/requests/api/v0/session_request_spec.rb
@@ -38,5 +38,58 @@ RSpec.describe "Session Request" do
         expect(parsed[:data][:attributes][:api_key]).to be_a(String)
       end
     end
+
+    context "when unsuccessful" do
+      describe "it returns a 401 status code" do
+        it "when the email params don't match an existing user" do
+          @user_params[:email] = "hello@hello.com"
+          post "/api/v0/sessions", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+  
+          expect(response.status).to eq(401)
+          expect(parsed[:error]).to eq("Credentials are Invalid")
+        end
+
+        it "when the password is wrong for the existing user's email" do
+          @user_params[:password] = "123"
+          post "/api/v0/sessions", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+  
+          expect(response.status).to eq(401)
+          expect(parsed[:error]).to eq("Credentials are Invalid")
+        end
+
+        it "when an email is formatted incorrectly" do
+          @user_params["email"] = "123"
+          post "/api/v0/sessions", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+          
+          expect(response.status).to eq(401)
+          expect(parsed[:error]).to eq("Credentials are Invalid")
+        end
+      end
+
+      describe "it returns a 400 status code" do
+        describe "when a field is missing" do
+          it "nil email" do
+            @user_params["email"] = nil
+            post "/api/v0/sessions", headers: @headers, params: @user_params.to_json
+            parsed = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response.status).to eq(400)
+            expect(parsed[:error]).to eq("One or more Credentials are missing")
+          end
+
+          it "nil password" do
+            @user_params["password"] = nil
+            post "/api/v0/sessions", headers: @headers, params: @user_params.to_json
+            parsed = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response.status).to eq(400)
+            expect(parsed[:error]).to eq("One or more Credentials are missing")
+          end
+        end
+      end
+    end
   end
 end
