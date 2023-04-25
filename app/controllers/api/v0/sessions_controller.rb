83cd160ffef23de19d7a0ea30ed26b475544bc67
Andra Helton <andrahelton@gmail.com>
2023-04-25 13:36:22 -0600

    feat: SessionsController#create sad path

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,16 +1,16 @@
 class Api::V0::SessionsController < ApplicationController
   
   def create
-    user = User.find_by(email: session_params[:email])
-    
     if session_params[:email].present? && session_params[:password].present?
+      user = User.find_by(email: session_params[:email])
+    
       if user != nil && user.authenticate(params[:password])
         render json: SessionSerializer.new(user), status: 200
       else
-        render json: { error: user.errors.full_messages.to_sentence }, status: 401
+        render json: { error: "Credentials are Invalid" }, status: 401
       end
     else
-      render json: { error: user.errors.full_messages.to_sentence }, status: 400
+      render json: { error: "One or more Credentials are missing" }, status: 400
     end
   end
 
