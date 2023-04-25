c20073431a4fa2ba6429e0d98be474ee223da83c
Andra Helton <andrahelton@gmail.com>
2023-04-24 22:27:17 -0600

    test: Create User request sad path

--- spec/requests/api/v0/user_request_spec.rb
+++ spec/requests/api/v0/user_request_spec.rb
@@ -33,5 +33,66 @@ RSpec.describe "user Request" do
         expect(parsed[:data][:attributes][:api_key]).to be_a(String)
       end
     end
+
+    context "when unsuccessful" do
+      describe "it returns a 400 status code" do
+        it "when password and pass confirmation don't match" do
+          @user_params["password_confirmation"] = "notthepassword"
+          post "/api/v0/users", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response.status).to eq(400)
+          expect(parsed[:error]).to eq("Password confirmation doesn't match Password")
+        end
+        
+        it "when email has been taken" do
+          User.create!(email: "whatever@example.com", password: "123", password_confirmation: "123")
+
+          post "/api/v0/users", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response.status).to eq(400)
+          expect(parsed[:error]).to eq("Email has already been taken")
+        end
+
+        it "when an email is formatted incorrectly" do
+          @user_params[:email] = "123"
+          post "/api/v0/users", headers: @headers, params: @user_params.to_json
+          parsed = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response.status).to eq(400)
+          expect(parsed[:error]).to eq("Email is invalid")
+        end
+        
+        describe "when a field is missing" do
+          it "nil email" do
+            @user_params["email"] = nil
+            post "/api/v0/users", headers: @headers, params: @user_params.to_json
+            parsed = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response.status).to eq(400)
+            expect(parsed[:error]).to eq("Email can't be blank and Email is invalid")
+          end
+
+          it "nil password" do
+            @user_params["password"] = nil
+            post "/api/v0/users", headers: @headers, params: @user_params.to_json
+            parsed = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response.status).to eq(400)
+            expect(parsed[:error]).to eq("Password can't be blank and Password can't be blank")
+          end
+
+          it "nil password_confirmation" do
+            @user_params["password_confirmation"] = nil
+            post "/api/v0/users", headers: @headers, params: @user_params.to_json
+            parsed = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(response.status).to eq(400)
+            expect(parsed[:error]).to eq("Password confirmation can't be blank")
+          end
+        end
+      end
+    end
   end
 end
