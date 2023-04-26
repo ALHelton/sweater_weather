315979cb13f871f4bb5bd13ab15f8e65c34a8ab9
Andra Helton <andrahelton@gmail.com>
2023-04-25 23:00:58 -0600

    feat: MapQuestService#get_travel_time

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -4,7 +4,7 @@ class WeatherService
     response = connection.get("/v1/forecast.json?q=#{lat},#{long}&days=5")
     JSON.parse(response.body, symbolize_names: true)
   end
-  
+
   private
   def connection
     Faraday.new("http://api.weatherapi.com") do |f|
