b1e5882b9d189d6725e6a22362569ac29556c95d
Andra Helton <andrahelton@gmail.com>
2023-04-26 00:13:50 -0600

    fix: Adjust line order in RoadTripFacade#trip_info

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,23 +2,22 @@ class RoadTripFacade
 
   def trip_info(start_point, end_point)
     service = MapQuestService.new.get_travel_time(start_point, end_point)
-    time_array = service[:route][:formattedTime].split(":")
     weather_info = WeatherFacade.new.forecast_json(end_point)
-
-    arrival_datetime = (Time.now + service[:route][:time]).to_s
-    datetime_segments = arrival_datetime.split(" ")
-    time_segments = datetime_segments[1].split(":")
-    arrival_time = "#{time_segments[0]}:#{time_segments[1]}"
-
-    hours = "#{time_array[0].to_i}h"
-    minutes = "#{time_array[1].to_i}m"
-
+    
     start_city = start_point
     end_city = end_point
     travel_time = "impossible route"
     weather_at_eta = {}
+    
+    if service[:info][:statuscode] == 0
+      time_array = service[:route][:formattedTime].split(":")
+      arrival_datetime = (Time.now + service[:route][:time]).to_s
+      datetime_segments = arrival_datetime.split(" ")
+      time_segments = datetime_segments[1].split(":")
+      arrival_time = "#{time_segments[0]}:#{time_segments[1]}"
+      hours = "#{time_array[0].to_i}h"
+      minutes = "#{time_array[1].to_i}m"
 
-    if service[:info][:statuscode] != 612
       found_arrival_day = weather_info[:forecast][:forecastday].find do |day|
         arrival_datetime.include?(day[:date])
       end
